25f50e540af7f8d828dc62c945a437e7b50cb916
Ahmad Abdallah <ahmadabdallah.r@gmail.com>
2019-04-04 18:08:08 +0200

    update config file

--- src/config/paymob.php
+++ src/config/paymob.php
@@ -25,7 +25,7 @@ return [
     |
     */
 
-    'api_key' => '',
+    'api_key' => env('PAYMOB_API_KEY'),
 
     /*
     |--------------------------------------------------------------------------
@@ -36,10 +36,24 @@ return [
     |
     */
 
+    'integration_id_credit' => env('PAYMOB_INTEGRATION_ID_CREDIT'),
+    'integration_id_cash' => env('PAYMOB_INTEGRATION_ID_CASH'),
+    'integration_id_wallet' => env('PAYMOB_INTEGRATION_ID_WALLET'),
+    'integration_id_aman' => env('PAYMOB_INTEGRATION_ID_AMAN'),
+    'iframe_id' => env('PAYMOB_IFRAME_ID'),
 
-    'integration_id_credit' => '',
-    'integration_id_cash' => '',
-    'integration_id_aman' => '',
-    'integration_id_wallet' => '',
-    'iframe_id' => '',
+
+    /*
+      |--------------------------------------------------------------------------
+      | PayMob integration Endpoints URL
+      |--------------------------------------------------------------------------
+      |
+      | This is  PayMob Endpoints URL.
+      |
+      */
+
+      'authentication_token_endpoint' => env('PAYMOB_AUTHENTICATION_TOKEN_ENDPOINT'),
+      'create_order_endpoint' => env('CREATE_ORDER_ENDPOINT'),
+      'payment_key_token_endpoint' => env('PAYMENT_KEY_TOKEN_ENDPOINT')
+
 ];
